0f190f90c264f9590925b42b032f193310e6b84a
Vinicius <Viniciusbss124@gmail.com>
2020-11-16 01:45:01 -0300

    struct to list
    struct to list

--- main.py
+++ main.py
@@ -21,20 +21,20 @@ if __name__ == '__main__':
                         new_k = old_key + '/' + partial_k
                         new_list = recursive_write(old_list, line, new_k, new_v)
                 elif isinstance(value, list):
-                    new_list = old_key * len(value)
-                    for index, new_v in enumerate(value):
-                        if isinstance(value, dict) or isinstance(value, list):
-                            new_list = recursive_write(old_list, line, old_key, new_v)
-                        else:
-                            copy_dict = line.copy()
-                            copy_dict[old_key] = new_v
-                            new_list[index] = copy_dict
+                    for new_line in old_list:
+                        for new_v in value:
+                            if isinstance(new_v, dict) or isinstance(new_v, list):
+                                new_list = recursive_write(old_list, line, old_key, new_v)
+                            else:
+                                copy_dict = new_line.copy()
+                                copy_dict[old_key] = new_v
+                                new_list.append(copy_dict)
                 else:
                     line[old_key] = value
                 return new_list or old_list
 
             for k, v in dict_file.items():
-                final_list = recursive_write(initial_list, initial_dict, k, v)
+                final_list = recursive_write(final_list or initial_list, initial_dict, k, v)
 
             csv_write = csv.DictWriter(csv_file, fieldnames=final_list[0].keys(), delimiter=';')
             csv_write.writeheader()
