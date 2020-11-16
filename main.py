c0392cad2a7428effc2f206a454d706bbcec45a4
Vinicius <Viniciusbss124@gmail.com>
2020-11-16 00:25:26 -0300

    structs test

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ if __name__ == '__main__':
     import json
     import csv
 
-    with open('result.json', encoding='utf-8') as json_file:
+    with open('someone_list.json', encoding='utf-8') as json_file:
         result = json_file.read()
         dict_file = json.loads(result)
 
@@ -14,51 +14,27 @@ if __name__ == '__main__':
             initial_list = [initial_dict]
             final_list = []
 
-            def control_recursive():
-                global count_recursive
-                count_recursive += 1
-                print(count_recursive)
-
-            def create_new_list(tmp_list, value):
-                control_iterator = len(value)
-                new_list = []
-                for n in range(0, control_iterator):
-                    for item in tmp_list:
-                        new_list.append(item.copy())
-                return new_list
-
-            def test_type_and_call_recursive(item, key, value, old_list):
+            def recursive_write(old_list, line, old_key, value):
                 new_list = []
                 if isinstance(value, dict):
-                    new_list = recursive_write(key, value, new_list or old_list)
+                    for partial_k, new_v in value.items():
+                        new_k = old_key + '/' + partial_k
+                        new_list = recursive_write(old_list, line, new_k, new_v)
                 elif isinstance(value, list):
-                    new_list = create_new_list(new_list or old_list, value)
-                    new_list = recursive_write(key, value, new_list or old_list)
+                    new_list = old_key * len(value)
+                    for index, new_v in enumerate(value):
+                        if isinstance(value, dict) or isinstance(value, list):
+                            new_list = recursive_write(old_list, line, old_key, new_v)
+                        else:
+                            copy_dict = line.copy()
+                            copy_dict[old_key] = new_v
+                            new_list[index] = copy_dict
                 else:
-                    if new_list:
-                        for new_line in new_list:
-                            new_line[key] = value
-                    else:
-                        item[key] = value
+                    line[old_key] = value
                 return new_list or old_list
 
-            def recursive_write(old_key, value_dict_or_list, old_list_lines):
-                control_recursive()
-                new_list_line = []
-                for line in old_list_lines:
-                    if isinstance(value_dict_or_list, dict):
-                        for new_k, value in value_dict_or_list.items():
-                            part_k = old_key + '/' + new_k
-                            new_list_line = test_type_and_call_recursive(line, part_k, value, new_list_line or old_list_lines)
-
-                    if isinstance(value_dict_or_list, list):
-                        for value in value_dict_or_list:
-                            new_list_line = test_type_and_call_recursive(line, old_key, value, new_list_line or old_list_lines)
-
-                return new_list_line or old_list_lines
-
             for k, v in dict_file.items():
-                final_list = recursive_write(k, v, final_list or initial_dict)
+                final_list = recursive_write(initial_list, initial_dict, k, v)
 
             csv_write = csv.DictWriter(csv_file, fieldnames=final_list[0].keys(), delimiter=';')
             csv_write.writeheader()
